68efc3e58b496ff74d08d0fd180e05c510538917
abreuzzzz <105026818+abreuzzzz@users.noreply.github.com>
2025-04-19 15:36:03 -0300

    Update and rename main.py to contas-a-pagar_17_20.py

--- contas-a-pagar_17_20.py
+++ contas-a-pagar_17_20.py
@@ -96,7 +96,7 @@ def main():
     centros = buscar_centros_de_custo(token)
 
     data_inicio = datetime.date(2016, 1, 1)
-    data_fim = datetime.date(2020, 1, 1)
+    data_fim = datetime.date(2021, 1, 1)
 
     while data_inicio < data_fim:
         inicio = data_inicio.strftime("%Y-%m-%d")
