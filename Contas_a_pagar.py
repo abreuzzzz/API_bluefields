977959bcf113e1a5a97a614c37a00b85cee2af40
abreuzzzz <105026818+abreuzzzz@users.noreply.github.com>
2025-04-29 17:52:54 -0300

    Update Contas_a_pagar.py

--- Contas_a_pagar.py
+++ Contas_a_pagar.py
@@ -1,20 +1,18 @@
 import requests
 import json
 
-url = 'https://services.contaazul.com/finance-pro-reader/v1/installment-view?page=1&page_size=10'
-
-headers = {
-    'X-Authorization': '00e3b816-f844-49ee-a75e-3da30f1c2630',
-    'Content-Type': 'application/json',
-}
+url = "https://services.contaazul.com/finance-pro-reader/v1/installment-view?page=1&page_size=1000"
 
 payload = json.dumps({
   "quickFilter": "ALL",
   "search": "",
   "type": "EXPENSE"
 })
+headers = {
+  'X-Authorization': '00e3b816-f844-49ee-a75e-3da30f1c2630',
+  'Content-Type': 'application/json',
+}
 
 response = requests.request("POST", url, headers=headers, data=payload)
 
-print(response.status_code)
-print(response.json())  # ou response.text se quiser ver o JSON bruto
+print(response.text)
