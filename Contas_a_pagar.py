84d10ca90d768690a1c84635dc946329b2201c4d
abreuzzzz <105026818+abreuzzzz@users.noreply.github.com>
2025-04-29 17:55:35 -0300

    Update Contas_a_pagar.py

--- Contas_a_pagar.py
+++ Contas_a_pagar.py
@@ -11,6 +11,7 @@ payload = json.dumps({
 headers = {
   'X-Authorization': '00e3b816-f844-49ee-a75e-3da30f1c2630',
   'Content-Type': 'application/json',
+  'User-Agent': 'Mozilla/5.0'
 }
 
 response = requests.request("POST", url, headers=headers, data=payload)
