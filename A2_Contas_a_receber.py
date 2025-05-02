1b052819363886dd3b5478eac9fd7e967c6ad7a2
abreuzzzz <105026818+abreuzzzz@users.noreply.github.com>
2025-05-02 18:47:51 -0300

    Update A2_Contas_a_receber.py

--- A2_Contas_a_receber.py
+++ A2_Contas_a_receber.py
@@ -72,8 +72,8 @@ dados_formatados = [extract_fields(item, colunas_base) for item in all_items]
 df = pd.DataFrame(dados_formatados)
 
 # ===================== Buscar ID da planilha no Google Drive =====================
-folder_id = "10UEE_tenpCEyJ_6dt2r1_iw7Vnpo9zm2"
-sheet_name = "Financeiro_contas_a_receber_Dagaz"
+folder_id = "1_kJtBN_cr_WpND1nF3WtI5smi3LfIxNy"
+sheet_name = "Financeiro_contas_a_receber_Bluefields"
 
 query = f"name='{sheet_name}' and mimeType='application/vnd.google-apps.spreadsheet' and '{folder_id}' in parents and trashed=false"
 results = drive_service.files().list(q=query, spaces='drive', fields="files(id, name)").execute()
