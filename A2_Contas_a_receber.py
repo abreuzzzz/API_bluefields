62aefea54580b34293232d07474628510ca5bdfc
abreuzzzz <105026818+abreuzzzz@users.noreply.github.com>
2025-11-14 11:36:55 -0300

    Update A2_Contas_a_receber.py

--- A2_Contas_a_receber.py
+++ A2_Contas_a_receber.py
@@ -152,7 +152,7 @@ colunas_renomear = {
     "Data de competência": "financialEvent.competenceDate",
     "Valor Calculado": "paid",
     "Centro de Custo 1": "categoriesRatio.costCentersRatio.0.costCenter",
-    "Categoria 1": "financialEvent.categoryDescriptions",
+    "Categoria 1": "categoriesRatio.category",
     "Descrição": "description",
     "Nome do cliente": "financialEvent.negotiator.name",
     "Data do último pagamento": "lastAcquittanceDate"
